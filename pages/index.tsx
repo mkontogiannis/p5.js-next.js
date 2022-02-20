a247751c4c13888d33a823c4c25ddf48fd3f1918
michael kontogiannis <mkontogiannis@protonmail.com>
2022-02-20 13:04:17 +0100

    feat: added SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,5 @@ import Head from 'next/head';
 import MySketch from '../components/MySketch';
 
 export default function Index() {
-  return (
-    <>
-      <Head>
-        <title>A p5js Nextjs/React Boilerplate</title>
-      </Head>
-      <MySketch />;
-    </>
-  );
+  return <MySketch />;
 }
